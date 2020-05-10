7e10b29d90a5fcd26f737fcee180e6a8f5571cbc
Arna <arnamaity6230@gmail.com>
2020-05-10 16:43:16 +0530

    Updated build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,3 @@
-# Change to your project
-cd tex/
-
 # Download the command wrapper and make it executable
 wget https://raw.githubusercontent.com/blang/latex-docker/master/latexdockercmd.sh
 chmod +x latexdockercmd.sh
@@ -9,4 +6,4 @@ chmod +x latexdockercmd.sh
 edit ./latexdockercmd.sh
 
 # Compile using pdflatex (docker will pull the image automatically)
-./latexdockercmd.sh pdflatex OE.tex
+./latexdockercmd.sh pdflatex tex/OE.tex
